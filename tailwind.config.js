2425c2e78019526c97221e3ac5de39547cea1340
abilnbl17 <abilabil17@gmail.com>
2025-08-07 14:05:06 +0700

    feat: update code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+const { heroui } = require("@heroui/react");
+
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  plugins: [
+    heroui({
+      theme: {
+        extend: {
+          colors: {
+            white: "#FFFFFF",
+            black: "#000000",
+            blue: {
+              50: "#e6f1fe",
+              100: "#cce3fd",
+              200: "#99c7fb",
+              300: "#66aaf9",
+              400: "#338ef7",
+              500: "#006FEE",
+              600: "#005bc4",
+              700: "#004493",
+              800: "#002e62",
+              900: "#001731",
+            },
+          },
+        },
+      },
+    }),
+  ],
 };
